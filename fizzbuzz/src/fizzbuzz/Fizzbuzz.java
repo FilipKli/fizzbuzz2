65420f3adcf728748c044d2ecaaadbcab54dd300
FilipKli <777filip@wp.pl>
2018-10-26 17:12:22 +0200

    zapis-1

--- fizzbuzz/src/fizzbuzz/Fizzbuzz.java
+++ fizzbuzz/src/fizzbuzz/Fizzbuzz.java
@@ -21,15 +21,14 @@ public class Fizzbuzz {
         
         for(int i=1;i<=checkCount;i++){
             
-            if((i%15)==0){
-                System.out.print("FizzBuzz");
-            }
-            else if((i%3)==0){
+            
+            if((i%3)==0){
                 System.out.print("Fizz");
             }
-            else if((i%5)==0){
+            if((i%5)==0){
                 System.out.print("Buzz");
             }
+            else if((i%3)==0);
             else {
                 System.out.print(i);
             }
